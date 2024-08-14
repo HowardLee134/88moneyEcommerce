aaf8dc03611757171fd7bb290b7e8bf881499474
HowardLee134 <m_lee134@u.pacific.edu>
2024-08-14 15:33:59 -0700

    Initial commit with all services

---
+++ item-service/src/test/java/com/_money/item/ItemServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com._money.item;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItemServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
